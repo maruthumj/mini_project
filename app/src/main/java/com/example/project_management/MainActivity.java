f4e3bfb122b00fbfb0755c8114a9f55deae3b3a5
maruthumj <maruthupandian72@gmail.com>
2020-09-12 17:52:56 +0530

    added fragments login,register,bottom navigation, firebase authentication,search activity,profile activity,home activity,notification activity,firestore,added forgot password feature.

--- app/src/main/java/com/example/project_management/MainActivity.java
+++ app/src/main/java/com/example/project_management/MainActivity.java
@@ -1,37 +1,114 @@
 package com.example.project_management;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
+//private FirebaseAuth fauth;
+    FirebaseFirestore fstore;
+    FirebaseAuth fauth;
+    String userid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
+        fauth=FirebaseAuth.getInstance();
+        //FirestoreProfileData();
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+          bottomNavigationView.setSelectedItemId(R.id.main);
+          bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+              @Override
+              public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                  switch(menuItem.getItemId()){
+                      case R.id.main:
+                          return true;
+                      case R.id.search_bar:
+                          startActivity(new Intent(getApplicationContext(),search.class));
+                            overridePendingTransition(0,0);
+                            return true;
+                      case R.id.notification:
+                          startActivity(new Intent(getApplicationContext(),notification.class));
+                          overridePendingTransition(0,0);
+                          return true;
+                      case R.id.profile:
+                          startActivity(new Intent(getApplicationContext(),profile.class));
+                          overridePendingTransition(0,0);
+                          return true;
 
-        NavController navController = Navigation.findNavController(this,  R.id.fragment);
 
+                  }
+return false;
+              }
+          });
+        }
+    @Override
+    protected void onStart()
+    {
+       super.onStart();
+        FirebaseUser fuser=fauth.getCurrentUser();
+        if(fuser != null)
+        {
 
-        NavigationUI.setupWithNavController( bottomNavigationView,navController);
-
-
+        }
+        else
+        {
+            startActivity(new Intent(this,login.class));
+            finish();
+        }
 
     }
+
+
+
+public void BottomNavigation()
+{
+
+
+
+}
+   /* public void FirestoreProfileData() {
+        fstore = FirebaseFirestore.getInstance();
+        userid = fauth.getCurrentUser().getUid();
+        profile prof=new profile();
+        Bundle b1=new Bundle();
+        FragmentManager manager = getSupportFragmentManager();
+        DocumentReference docref = fstore.collection("users").document(userid);
+        docref.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                String name, phone, email;
+                assert value != null;
+                name = value.getString("name");
+                phone = value.getString("phone");
+                email = value.getString("email");
+                b1.putString("name2", name);
+                b1.putString("phone2", phone);
+                b1.putString("email2", email);
+                prof.setArguments(b1);
+            }
+        });
+    }*/
 }
